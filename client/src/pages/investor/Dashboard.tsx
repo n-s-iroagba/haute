10fefaf0d31975bea929c6ffa42009545dd2ced4
n-s-iroagba <nnamdisolomon1@gmail.com>
2024-08-14 20:53:28 +0100

    [chore:clean up]

--- client/src/pages/investor/Dashboard.tsx
+++ client/src/pages/investor/Dashboard.tsx
@@ -140,7 +140,9 @@ const Dashboard: React.FC<{username: string; id: number}> = ({username, id}) => 
       </>
     )
   }
+  <div className='w-100 d-flex justify-content-center'>
     <MiniFooter />
+    </div>
        </>
   );
   
